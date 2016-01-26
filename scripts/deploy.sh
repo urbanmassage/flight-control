b08e39d10f4062d24a5a9b0687fc575b692da81f
Louy <louy08@gmail.com>
2016-01-26 22:28:07 +0000

    deploy info messages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,19 +3,23 @@
 set -e
 
 if [ -z "$DOCKER_EMAIL" ] || [ -z "$DOCKER_USER" ] || [ -z "$DOCKER_PASS" ]; then
-  echo "Please set DOCKER_EMAIL, DOCKER_USER and DOCKER_PASS first"
+  echo "[deploy] Please set DOCKER_EMAIL, DOCKER_USER and DOCKER_PASS first"
   exit 1
 fi
 
-echo "removing dev deps"
+echo "[deploy] removing dev deps"
 npm prune --production
 
+echo "[deploy] building docker image"
 docker build --no-cache -t "$TRAVIS_REPO_SLUG" .
 
+echo "[deploy] tagging docker image"
 docker tag "$TRAVIS_REPO_SLUG" "${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}"
 docker tag "$TRAVIS_REPO_SLUG" "${TRAVIS_REPO_SLUG}:${TRAVIS_COMMIT}"
 docker tag "$TRAVIS_REPO_SLUG" "${TRAVIS_REPO_SLUG}:travis-${TRAVIS_BUILD_NUMBER}"
 
+echo "[deploy] pushing docker image"
 docker push "$TRAVIS_REPO_SLUG"
 
+echo "[deploy] success!"
 docker images
