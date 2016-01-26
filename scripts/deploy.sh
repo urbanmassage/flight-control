18e1bfb1eff95eb0773f9227f3605109191382ac
Louy <louy08@gmail.com>
2016-01-26 17:57:09 +0000

    Update deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+npm prune --production
+
 if [ -z "$DOCKER_EMAIL" ] || [ -z "$DOCKER_USER" ] || [ -z "$DOCKER_PASS" ]; then
   echo "Please set DOCKER_EMAIL, DOCKER_USER and DOCKER_PASS first"
   exit 1
@@ -10,3 +14,5 @@ docker tag "$TRAVIS_REPO_SLUG" "${TRAVIS_REPO_SLUG}:${TRAVIS_COMMIT}"
 docker tag "$TRAVIS_REPO_SLUG" "${TRAVIS_REPO_SLUG}:travis-${TRAVIS_BUILD_NUMBER}"
 
 docker push $TRAVIS_REPO_SLUG
+
+docker images
