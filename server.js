87b985d3a57dcd5853fdd11f8cc7a89d479846f5
gileze33 <giles.williams@gmail.com>
2016-05-03 09:42:34 +0100

    Remove stupid logging in server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var server = new Server({
   controllers: './app/controllers',
   middleware: './app/middleware',
   debug: console.log,
-  autoStart: false
+  autoStart: false,
+  cors: true,
 });
 
 server.server.set('view engine', 'ejs');
@@ -20,23 +21,6 @@ function setup() {
   server.get('/favicon.ico', function (req, res) { res.end(); });
 
   server.use(require('cookie-parser')());
-
-  server.use(function (req, res, next) {
-    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.header('Pragma', 'no-cache');
-    res.header('Expires', 0);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', req.header('Access-Control-Request-Headers'));
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-
-    if (req.method === 'OPTIONS') {
-      return res.send(200);
-    }
-
-    console.log(req.method, req.path);
-
-    next();
-  });
 
   server.use(require('./app/lib/mongo').express);
 
