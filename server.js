39651d11c561f86d924dcf44b83052176b093af0
Louay Alakkad <louay@alakkad.me>
2016-07-23 16:13:13 +0100

    Update easy-server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 process.title = 'node flight-control';
 
-var Server = require('easy-server');
+var easyServer = require('easy-server');
+
+if (process.env.NODE_ENV === 'development') {
+  easyServer = require('easy-server-dev');
+}
+
 var hata = require('hata');
 
-var server = new Server({
+var app = easyServer({
   port: process.env.PORT || 1984,
   controllers: './app/controllers',
   middleware: './app/middleware',
@@ -12,38 +17,54 @@ var server = new Server({
   cors: true,
 });
 
-server.server.set('view engine', 'ejs');
-server.server.set('views', 'app/views');
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
-function setup() {
-  server.use(require('serve-static')('./public'));
-  server.use('/client/*', function (req, res, next) { next(hata(404)); });
-  server.get('/favicon.ico', function (req, res) { res.end(); });
+if (process.env.HOT) {
+  var debounce = require('js-debounce');
+  var webpack = require('webpack');
+  var config = require('./webpack.config');
+  var compiler = webpack(config);
 
-  server.use(require('cookie-parser')());
+  // Serve hot-reloading bundle to client
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  }));
+  app.use(require('webpack-hot-middleware')(compiler));
 
-  server.use(require('./app/lib/mongo').express);
-
-  server.start();
-
-  server.use(function (error, req, res, next) {
-    console.error(error);
-
-    var output = JSON.parse(JSON.stringify(error));
-    output.name = error.name;
-    output.message = error.message;
-
-    res.status(500).send(output);
-  });
-
-  server.use(function (req, res, next) {
-    require('./app/client/server-render')(req, res, next);
+  // Do "hot-reloading" of react stuff on the server
+  // Throw away the cached client modules and let them be re-required next time
+  compiler.plugin('done', function () {
+    debounce('clear-client-cache', 1000, function () {
+      console.log("Clearing client module cache");
+      Object.keys(require.cache).forEach(function(id) {
+        if (/\/client\//.test(id)) delete require.cache[id];
+      });
+    });
   });
 }
 
-if (require.main === module) {
-  setup();
-} else {
-  exports.app = server;
-  exports.setup = setup;
-}
+app.use(require('serve-static')('./public'));
+app.use('/client/*', function (req, res, next) { next(hata(404)); });
+app.get('/favicon.ico', function (req, res) { res.end(); });
+
+app.use(require('cookie-parser')());
+
+app.use(require('./app/lib/mongo').express);
+
+app.start();
+
+app.use(function (req, res, next) {
+  require('./app/client/server-render')(req, res, next);
+});
+
+app.use(function (error, req, res, next) {
+  console.error(error);
+
+  var output = JSON.parse(JSON.stringify(error));
+  output.name = error.name;
+  output.message = error.message;
+
+  res.status(500).send(output);
+});
