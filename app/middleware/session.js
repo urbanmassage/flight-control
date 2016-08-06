5a3fdb0e2f6b1e3cff5cedb9f342d19472e380da
Louy <louy08@gmail.com>
2016-08-06 10:10:48 +0100

    NO_AUTH option

--- app/middleware/session.js
+++ app/middleware/session.js
@@ -1,17 +1,28 @@
 var session = require('../lib/session');
+var NO_AUTH = process.env.NO_AUTH;
 
-module.exports = function(req, res, next) {
-  session.get(req.cookies['panel_login_token'], function(err, session) {
-    if(err) {
-      return next(err);
-    }
+var sessionMiddleware;
 
-    if(!session) {
-      return res.redirect('/login');
-    }
-
-    req.session = session.data;
-
+if (NO_AUTH) {
+  sessionMiddleware = function(req, res, next) {
     next();
-  });
-};
+  }
+} else {
+  sessionMiddleware = function(req, res, next) {
+    session.get(req.cookies['panel_login_token'], function(err, session) {
+      if (err) {
+        return next(err);
+      }
+
+      if (!session) {
+        return res.redirect('/login');
+      }
+
+      req.session = session.data;
+
+      next();
+    });
+  };
+}
+
+module.exports = sessionMiddleware;
